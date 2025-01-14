86e664a850389c1234ba2ac41ed75c56f6d06278
aviadkim <aviad@kimfo-fs.com>
2025-01-14 09:14:14 +0200

    Update Firebase configuration

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,23 +1,18 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-import { getAuth, GoogleAuthProvider } from 'firebase/auth';
-import { getStorage } from 'firebase/storage';
+import { getAuth } from 'firebase/auth';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBH5dVh9kYjIgZU9dI2UcpQ6pTIKDPfXZM",
-  authDomain: "finance-5039e.firebaseapp.com",
-  projectId: "finance-5039e",
-  storageBucket: "finance-5039e.appspot.com",
-  messagingSenderId: "903837426191",
-  appId: "1:903837426191:web:b99d9c0162415f31cd7856"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
-const storage = getStorage(app);
-const googleProvider = new GoogleAuthProvider();
 
-export { app, db, auth, storage, googleProvider };
+export { db, auth };
