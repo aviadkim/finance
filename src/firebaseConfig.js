efe603533ebd4c56e732b7c3c2a43663cd83d9ff
aviadkim <aviad@kimfo-fs.com>
2025-01-14 09:22:08 +0200

    Restore working Firebase configuration

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: "AIzaSyC2Ak5YzaXi_3QR4yQXtx9D9Y4k6lHvzJc",
+  authDomain: "finance-bfc48.firebaseapp.com",
+  projectId: "finance-bfc48",
+  storageBucket: "finance-bfc48.appspot.com",
+  messagingSenderId: "41592992981",
+  appId: "1:41592992981:web:c016b47bfa7f13523c913f"
 };
 
 const app = initializeApp(firebaseConfig);
