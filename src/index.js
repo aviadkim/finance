bfc7b39e6b7c6f53bf32909cef95c73d08c867c2
aviadkim <aviad@kimfo-fs.com>
2025-01-20 23:23:41 +0200

    Update index.js with clean setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
+console.log('Application starting... v1.0.4');
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
